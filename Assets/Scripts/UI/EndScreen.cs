53df781d84678cd41fc60c3ffe313bc3d3cb0fca
Perz <s.ali224@mybvc.ca>
2023-11-17 08:17:00 -0700

    WinLoseScreen

---
+++ Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndScreen : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void RestartGame()
+    {
+       SceneManager.LoadScene(1);
+    }
+
+}
