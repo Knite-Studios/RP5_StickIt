ed820bf119be062ce5c3001ae9208eebfc895a43
Musse <musse.hidru@outlook.com>
2023-11-23 21:26:22 -0700

    removed multi task system

--- Assets/Scripts/MISC/ObjectiveManager.cs
+++ Assets/Scripts/MISC/ObjectiveManager.cs
@@ -5,29 +5,30 @@ using TMPro;
 public class ObjectiveManager : MonoBehaviour
 {
     public static ObjectiveManager Instance; // Singleton pattern to access it globally
-    [Header("Objectives"),SerializeField]
-    List<Objective> mainObjectives = new ();
-    public List<Objective> sideObjectives = new ();
+
+    public Objective mainObjective;
+    public Objective sideObjective;
     public TextMeshProUGUI mainObjectiveText; // Reference to the TextMeshPro field for main objectives
     public TextMeshProUGUI sideObjectiveText; // Reference to the TextMeshPro field for side objectives
 
-    private Dictionary<CropType, int> collectiblesCollected = new ();
-
+    private Dictionary<CropType, int> collectiblesCollected = new Dictionary<CropType, int>();
     private int stars = 0;
-    private bool givestar1 = true;
-    private bool givestar2 = true;
+    private bool giveStarMain1 = true;
+    private bool giveStarSide2 = true;
+    private bool giveStarSide3 = true;
+
     public bool UnlockDoor()
     {
-        return ObjectiveCompleted(mainObjectives[0]);
+        return ObjectiveCompleted(mainObjective);
     }
-    private bool ObjectiveCompleted(Objective obj) 
+
+    private bool ObjectiveCompleted(Objective obj)
     {
         if (collectiblesCollected[obj.collectibleType] >= obj.requiredAmount)
         {
             return true;
         }
         return false;
-   
     }
 
     private void Awake()
@@ -40,102 +41,84 @@ public class ObjectiveManager : MonoBehaviour
 
     private void InitializeCollectibles()
     {
-        // Initialize the dictionary with all collectibles
-        foreach (Objective objective in mainObjectives)
-        {
-            collectiblesCollected.Add(objective.collectibleType, 0);
-        }
-        foreach (Objective objective in sideObjectives)
-        {
-            collectiblesCollected.Add(objective.collectibleType, 0);
-        }
+        collectiblesCollected.Add(mainObjective.collectibleType, 0);
+        collectiblesCollected.Add(sideObjective.collectibleType, 0);
     }
+
     public void HandleCropDestroyed(CropType destroyedCropType)
     {
         if (collectiblesCollected.ContainsKey(destroyedCropType))
         {
             collectiblesCollected[destroyedCropType]++;
-            // Check if objectives are completed
-            foreach (Objective objective in mainObjectives)
-            {
-                if (objective.collectibleType == destroyedCropType)
-                {
-                    CheckObjectiveCompletion(objective);
-                    UpdateObjectiveText();
-                    break;
-                }
-            }
-            foreach (Objective objective in sideObjectives)
-            {
-                if (objective.collectibleType == destroyedCropType)
-                {
-                    CheckObjectiveCompletion(objective);
-                    if ((collectiblesCollected[objective.collectibleType] >= (objective.requiredAmount/2)) && givestar1)
-                    {
-                        stars++;
-                        givestar1 = false;
-                    }
-                    UpdateObjectiveText();
-                    break;
-                }
-            }
+
+            CheckObjectiveCompletion(mainObjective, ref giveStarMain1);
+            CheckObjectiveCompletion(sideObjective, ref giveStarSide2);
+            CheckSideObjectiveHalf(sideObjective, ref giveStarSide3);
+            UpdateObjectiveText();
         }
     }
 
-    private void CheckObjectiveCompletion(Objective objective)
+    private void CheckObjectiveCompletion(Objective objective, ref bool giveStar)
     {
         if (collectiblesCollected[objective.collectibleType] >= objective.requiredAmount)
         {
-            if (givestar2)
-            {
-                stars++;
-                givestar2 = false;
-            }
-            
-            // Objective completed
+
             Debug.Log($"Objective completed: Gathered {objective.requiredAmount} {objective.collectibleType.name}");
 
-            // Handle UI or other actions for completed objective
+            if (giveStar)
+            {
+                stars++;
+                giveStar = false;
+            }
+        }
+    }
+    private void CheckSideObjectiveHalf(Objective objective, ref bool giveStar)
+    {
+        if ((collectiblesCollected[objective.collectibleType] >= (objective.requiredAmount)/2))
+        {
+            if (giveStar)
+            {
+                stars++;
+                giveStar = false;
+            }
         }
     }
 
     private void UpdateObjectiveText()
     {
-        // Update the text for the main and side objectives
-        if (ObjectiveCompleted(mainObjectives[0]))
+        if (ObjectiveCompleted(mainObjective))
         {
             mainObjectiveText.text = "Main objective completed!";
-
         }
-
         else
-        UpdateTextForObjectiveType(mainObjectives, mainObjectiveText);
-
-        if (ObjectiveCompleted(sideObjectives[0]))
-            sideObjectiveText.text = "Side objective completed!";
-        else
-        UpdateTextForObjectiveType(sideObjectives, sideObjectiveText);
-    }
-
-    private void UpdateTextForObjectiveType(List<Objective> objectives, TextMeshProUGUI textMesh)
-    {
-        // Update the text for the given objective type
-        string objectivesString = "";
-        foreach (Objective objective in objectives)
         {
-            string collectibleName = objective.collectibleType.name;
-            int collectedAmount = collectiblesCollected[objective.collectibleType];
-            int requiredAmount = objective.requiredAmount;
-            objectivesString += $"{collectibleName}: {collectedAmount}/{requiredAmount}\n";
+            UpdateTextForObjective(mainObjective, mainObjectiveText);
         }
-        textMesh.text = objectivesString;
- 
+
+        if (ObjectiveCompleted(sideObjective))
+        {
+            sideObjectiveText.text = "Side objective completed!";
+        }
+        else
+        {
+            UpdateTextForObjective(sideObjective, sideObjectiveText);
+        }
     }
+
+    private void UpdateTextForObjective(Objective objective, TextMeshProUGUI textMesh)
+    {
+        string collectibleName = objective.collectibleType.name;
+        int collectedAmount = collectiblesCollected[objective.collectibleType];
+        int requiredAmount = objective.requiredAmount;
+        textMesh.text = $"{collectibleName}: {collectedAmount}/{requiredAmount}\n";
+    }
+
     public int StarCount()
     {
-        return stars;  
+        return stars;
     }
 }
+
 [System.Serializable]
 public class Objective
 {
